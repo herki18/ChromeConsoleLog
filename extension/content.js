291f45a2c797beef4cd5b003f94037e0875c53fa
unknown <parnh@LAPTOP-HERKI.adastra.co.uk>
2015-09-08 18:36:27 +0100

    extension initial commit

---
+++ extension/content.js
@@ -0,0 +1,89 @@
+var JSErrorCollector = {
+	getSavedErrors: function() {
+		var savedErrors = [];
+		if (!!localStorage.JSErrors)
+				savedErrors = JSON.parse(localStorage.JSErrors);
+
+		return savedErrors;
+	},
+	saveErrors: function(errors) {
+		localStorage.JSErrors = JSON.stringify(errors);
+	},
+	push: function (jsErrors) {
+		var list = this.getSavedErrors();
+		list.push(jsErrors);
+		this.saveErrors(list);
+	},
+	pump: function() {
+		var list = this.getSavedErrors();
+		this.clear();
+		return list;
+	},
+	clear: function() {
+		this.saveErrors([]);
+	},
+	onError: function(errorMessage, sourceName, lineNumber) {
+		
+		if(!errorMessage.message) {
+			return;
+		}
+		if(!!errorMessage.filename) {
+			sourceName = errorMessage.filename;
+		}
+		if(!!errorMessage.lineno) {
+			lineNumber = errorMessage.lineno;
+		}
+
+		if(!!errorMessage.target.chrome && !sourceName && !lineNumber && errorMessage.message != 'Script error.') {
+			return;
+		}	
+
+		errorMessage = errorMessage.message;
+		if(errorMessage == 'Script error.') {
+			var error = {
+				errorMessage: errorMessage,
+				sourceName: '',
+				lineNumber: 0,
+				pageUrl: document.location.href	
+			}
+		} else {
+			var error = {
+				errorMessage: errorMessage.replace(/^Uncaught /g, ''),
+				sourceName: sourceName,
+				lineNumber: lineNumber,
+				pageUrl: document.location.href	
+			}
+		}
+
+		JSErrorCollector.push(error);
+	},
+	consol: function() {
+		var oldf = console.log; 
+		console.log = function()
+		{ 
+			JSErrorCollector.push(arguments); 
+			oldf.apply(console, arguments); 
+		};
+	},
+	initialize: function() {
+		window.addEventListener('error', JSErrorCollector.onError, false);
+		
+		var script   = document.createElement("script");
+		script.type  = "text/javascript";
+		script.textContent = 'console.log("tere"); var loggs = []; var oldf = console.log; console.log = function() { loggs.push(arguments); oldf.apply(console, arguments); };';
+		script.async = false;
+		(document.head||document.documentElement).appendChild (script);
+		
+		var s = document.createElement('script');
+		s.src = chrome.extension.getURL('error_listener.js');
+		(document.head||document.documentElement).appendChild(s);
+		s.onload = function() {
+			s.parentNode.removeChild(s);
+			
+		};
+	}
+};
+
+
+JSErrorCollector.initialize();
+
